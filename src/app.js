2068a581ce88838fc8380ed74556de5ad1f2866b
Yash Chopra <choprayash601@gmail.com>
2026-04-08 11:00:51 +0530

    exploring get post use

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 
-const app= express()
+const app = express();
 
-app.use((req,res)=>{
+// app.use("/", (req, res) => {
+//     res.send("Hello fr");
+// });
 
-    res.send("Heo fr")
+app.use("/test", (req, res) => {
+    res.send("Hello fr test");
+});
+
+
+
+app.get("/user", (req, res) => {
+    res.send({ firstname: "Yash", lastname: "Chopra" });
+});// this will  oly handle gwt calls to get /user
+
+app.post("/user" ,(req,res)=>{
+
+    res.send("hello to the server")
 })
 
 
-app.listen(3000,()=>{
-    console.log("if listening on port");
-    
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
 });
